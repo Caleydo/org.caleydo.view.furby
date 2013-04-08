f5fbf1009f070bdc7f04c5ec45eb93545d46b190
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-04-08 08:32:55 +0000

    glgraphics:  * generalize context local objects (text, texture, tesselator) to own class  * add TesselatedPolygon to GLGraphics -> Band computation and rendering can be splitted
    git-svn-id: svn://localhost:8080/bicluster/trunk@68 6e201009-ea7f-4747-b7f3-d4bff3160859

--- src/org/caleydo/view/bicluster/util/SimpleBandTest.java
+++ src/org/caleydo/view/bicluster/util/SimpleBandTest.java
@@ -1,57 +1,44 @@
 package org.caleydo.view.bicluster.util;
 
-import java.awt.geom.Point2D;
+import gleem.linalg.Vec3f;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.media.opengl.GLContext;
-
 import org.caleydo.core.util.collection.Pair;
-import org.caleydo.core.util.color.Colors;
 import org.caleydo.core.view.opengl.layout2.GLElement;
 import org.caleydo.core.view.opengl.layout2.GLGraphics;
 import org.caleydo.core.view.opengl.layout2.GLSandBox;
-import org.caleydo.core.view.opengl.layout2.IGLElementContext;
 import org.caleydo.core.view.opengl.layout2.layout.GLPadding;
-import org.caleydo.core.view.opengl.util.spline.ConnectionBandRenderer;
+import org.caleydo.core.view.opengl.util.spline.Band;
+import org.caleydo.core.view.opengl.util.spline.TesselatedPolygons;
 
 /**
  * @author Samuel Gratzl
  *
  */
 public class SimpleBandTest extends GLElement {
-	private ConnectionBandRenderer r = new ConnectionBandRenderer();
-
-	@Override
-	protected void init(IGLElementContext context) {
-		super.init(context);
-		r = new ConnectionBandRenderer();
-		r.init(GLContext.getCurrentGL().getGL2());
-	}
-
-	@Override
-	protected void takeDown() {
-		r = null;
-		super.takeDown();
-	}
 
 	@Override
 	protected void renderImpl(GLGraphics g, float w, float h) {
 
-		boolean highlight = false;
-		float[] color = Colors.BLUE.getRGBA();
-		List<Pair<Point2D, Point2D>> points = new ArrayList<>();
+		List<Pair<Vec3f, Vec3f>> points = new ArrayList<>();
 		// point pairs defining line segments
 		points.add(pair(100, 0, 120, 10));
 		points.add(pair(100 + 70, 200, 120 + 70, 200));
 		points.add(pair(100 + 70, 300, 120 + 70, 300));
 		points.add(pair(100 + 20, 410, 120 + 20, 400));
-		r.renderComplexBand(g.gl, points, highlight, color, .5f);
+
+		// can be stored :)
+		Band band = TesselatedPolygons.band(points);
+
+		g.color(0, 0, 1, 0.5f);
+		g.fillPolygon(band);
 	}
 
-	private Pair<Point2D, Point2D> pair(float x1, float y1, float x2, float y2) {
-		Point2D _1 = new Point2D.Float(x1, y1);
-		Point2D _2 = new Point2D.Float(x2, y2);
+	private Pair<Vec3f, Vec3f> pair(float x1, float y1, float x2, float y2) {
+		Vec3f _1 = new Vec3f(x1, y1, 0);
+		Vec3f _2 = new Vec3f(x2, y2, 0);
 		return Pair.make(_1, _2);
 	}
 
