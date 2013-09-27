78dbaaf5e0c09e21c43601caa3ebff5fb3f4b118
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-09-27 13:12:49 +0200

    fix toolbox sizes

--- src/org/caleydo/view/bicluster/elem/toolbar/ParameterToolBarElement.java
+++ src/org/caleydo/view/bicluster/elem/toolbar/ParameterToolBarElement.java
@@ -504,7 +504,7 @@ public class ParameterToolBarElement extends AToolBarElement implements MyUnboun
 	 */
 	@Override
 	public Rect getPreferredBounds() {
-		return new Rect(-205, 0, 200, 365 + 20);
+		return new Rect(-205, 0, 200, 400 + 20);
 	}
 
 	private static class MyTextRender implements IGLRenderer {
